91d01e3bf34f8f59d3c3f161b93feea6e9b491c2
odhis101 <joshodhiambo5@gmail.com>
2022-10-15 11:07:03 +0300

    working on post a job page

--- src/pages/PostAJob/PostAJob.js
+++ src/pages/PostAJob/PostAJob.js
@@ -6,31 +6,26 @@ export default function PostAJob() {
          <>
         <p> HOME | POST A JOB </p>
         <div className="centerContainer">
-        <div class="flex justify-center">
-  <div class="mb-3 xl:w-96">
-    <select class="form-select appearance-none
-      block
-      w-full
-      px-3
-      py-1.5
-      text-base
-      font-normal
-      text-gray-700
-      bg-white bg-clip-padding bg-no-repeat
-      border border-solid border-gray-300
-      rounded
-      transition
-      ease-in-out
-      m-0
-      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
-        <option selected>Open this select menu</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
-    </select>
-  </div>
+       
+        {   /*
+        <label for="countries" class="">Categories</label>
+        <div class="flex  ">
+    <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block
+     w-1/3  my-3 p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black ">
+  <option selected>Choose a country</option>
+  <option value="US">United States</option>
+  <option value="CA">Canada</option>
+  <option value="FR">France</option>
+  <option value="DE">Germany</option>
+</select>
+<input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"></input>
+</div>  
+
+    */}
 </div>
-            </div>
+
+
+
 
 
          </>
